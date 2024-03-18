031caedbb668ec3e33ee457e93aa1a6b2e376d9a
EduardoCostadaSilva <eduardocosta5556@gmail.com>
2024-03-18 17:01:06 -0300

    commit final

--- back-end/app/configs/auth.config.js
+++
@@ -1,4 +0,0 @@
-//Palavrinha secreta (secret
-module.exports = {
-  secret: "Whatsapp-chave-secreta",//private key
-};
--- back-end/app/configs/db.config.js
+++
@@ -1,9 +0,0 @@
-//Arquivo de configurações do BD
-
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSOWORD: "",
-  DB: "xdev",
-  PORT: "3306",
-};
